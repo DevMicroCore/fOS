2369f161c3e0337563544c87c50ff2a47a61d481
Felix Leopold Meinhardt <meinhardt-felix@web.de>
2026-05-24 19:00:00 +0200

    Bugfix - V2.2.1

--- Crowpanel_7"_esp32s3/fOS2.0/ui_ScreenSettings.c
+++ Crowpanel_7"_esp32s3/fOS2.0/ui_ScreenSettings.c
@@ -204,7 +204,7 @@ lv_obj_set_height( ui_LabelSoftwareInformationData, 120);
 lv_obj_set_x( ui_LabelSoftwareInformationData, 235 );
 lv_obj_set_y( ui_LabelSoftwareInformationData, 165 );
 lv_obj_set_align( ui_LabelSoftwareInformationData, LV_ALIGN_CENTER );
-lv_label_set_text(ui_LabelSoftwareInformationData,"fOS version:\n2.2.0\nDeveloper: \nF. L. Meinhardt");
+lv_label_set_text(ui_LabelSoftwareInformationData,"fOS version:\n2.2.1\nDeveloper: \nF. L. Meinhardt");
 lv_obj_set_style_text_font(ui_LabelSoftwareInformationData, &lv_font_montserrat_20, LV_PART_MAIN| LV_STATE_DEFAULT);
 
 ui_ButtonSettingsGeneral = lv_btn_create(ui_ScreenSettings);
